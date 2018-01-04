e11973f7bf950aa95864937ff765e16e25a2d6dc
Ed Dore <eddo@microsoft.com>
2018-01-04 12:47:05 -0800

    CppDLLProjectTemplate sample
    A simple C++ DLL project template, with custom IWizard assembly that correctly sets the WindowsTargetPlatformVersion propertly and includes a .vcxproj.filters file.

---
+++ CppDLLProjectTemplate/SimpleCppDLLTemplate/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SimpleCppDLLTemplate")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ed Dore")]
+[assembly: AssemblyProduct("SimpleCppDLLTemplate")]
+[assembly: AssemblyCopyright("Copyright © Ed Dore 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("604e9e97-021b-42dc-b488-0a953f93083e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CppDLLProjectTemplate/SimpleCppDLLWizard/SimpleCppDLLWizard.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+using Microsoft.VisualStudio.VCProjectEngine;
+
+namespace SimpleCppDLLWizard
+{
+    public class Wizard : IWizard
+    {
+        private DTE _dte;
+
+        public void BeforeOpeningFile(ProjectItem projectItem) { }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+            // use VCProject.LatestTargetPlatformVersion property, which is what the stock wizards use.
+            VCProject vcProject = (VCProject)project.Object;
+            string wtpv = vcProject.LatestTargetPlatformVersion;
+
+            if (wtpv != null)
+            {
+                // we only have to do this for a single config, as the property in question is global.
+                IVCCollection configs = (IVCCollection)vcProject.Configurations;
+                VCConfiguration firstConfig = (VCConfiguration)configs.Item(1);
+                IVCRulePropertyStorage rule = (IVCRulePropertyStorage)firstConfig.Rules.Item("ConfigurationGeneral");
+                rule?.SetPropertyValue("WindowsTargetPlatformVersion", wtpv);
+            }
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem) { }
+        public void RunFinished() { }
+
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+            _dte = (DTE)automationObject;
+
+            // manually copy over the SimpleDLL.vcxproj.filters file because it isn't
+            // referenced by the .vcxproj, and the wizard engine doesn't copy it to
+            // the project's destination directory.
+
+            // note, customParams[0] is the full path to the .vstemplate file, so we'll
+            // use it's folder to find the needed SimpleDLL.vcxproj.filters file.
+
+            string sourceFile = Path.Combine(Path.GetDirectoryName((string)customParams[0]), "SimpleDll.vcxproj.filters");
+            string destFile = Path.Combine(replacementsDictionary["$destinationdirectory$"], string.Format("{0}.vcxproj.filters", replacementsDictionary["$projectname$"]));
+            File.Copy(sourceFile, destFile);
+        }
+
+        public bool ShouldAddProjectItem(string filePath) { return true; }
+    }
+}
